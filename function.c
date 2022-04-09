812d15365dac9521a7d0dc9ae24bda844ed050ec
cgzhang <cg__zhang@163.com>
2022-04-09 16:30:02 +0800

    接口和数据类型说明

--- function.c
+++ function.c
@@ -174,3 +174,36 @@ int connect(int sock, struct sockaddr *servaddr, socklen_t addrlen);
 	servaddr: 保存目标服务器端地址信息的变量地址值
 	addrlen: 以字节为单位传递已传递给第二个结构体参数servaddr的地址变量长度
 */
+
+
+// 基于UDP的数据I/O函数
+#include <sys/socket.h>
+
+// 发送消息，在此阶段给套接字分配IP(主机)和端口号
+ssize_t sendto(int sock, void *buff, size_t nbytes, int flags, struct sockaddr *to, socklen_t addrlen);
+        // 成功时返回传输的字节数，失败返回-1
+
+/*
+	sock: 用于传输数据的UDP套接字文件描述符
+	buff: 保存待传输数据的缓冲地址值
+	nbytes: 待传输的数据长度，以字节为单位
+	flags: 可选项参数，若没有则传递0
+	to: 存有目标地址信息的sockaddr结构体变量的地址值
+	addrlen: 传递给参数to的地址值结构体变量长度
+*/
+
+
+// 接收发送端消息
+#include <sys/socket.h>
+
+ssize_t recvfrom(int sock, void *buff, size_t nbytes, int flags, struct sockaddr *from, socklen_t *addrlen);
+        // 成功时返回接收的字节数，失败返回-1
+
+/*
+	sock: 用于传输数据的UDP套接字文件描述符
+	buff: 保存接收数据的缓冲地址值
+	nbytes: 可接受的最大字节数
+	flags: 可选项参数，若没有则传递0
+	from: 存有发送端地址信息的sockaddr结构体变量的地址值
+	addrlen: 保存参数为from的结构体变量长度的变量地址值
+*/
