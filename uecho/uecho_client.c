4d59d007ac0431e0d189886bf7a9cd4ae49427a5
cgzhang <cg__zhang@163.com>
2022-04-09 16:28:05 +0800

    基于UDP的回声客户端/服务端

---
+++ uecho/uecho_client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define BUF_SIZE 30
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	// socklen_t adr_sz;
+	
+	struct sockaddr_in serv_adr, from_adr; // 不再需要from_adr,建立在已连接套接字上，每次目标固定
+	if (argc != 3)
+	{
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == -1)
+		error_handling("socket() error");
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = PF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+	connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)); // 创建已连接UDP套接字
+
+	while (1)
+	{
+		fputs("Insert message(q to quit): ", stdout);
+		fgets(message, sizeof(message), stdin);
+		if ((!strcmp(message, "q\n")) || (!strcmp(message, "Q\n")))
+			break;
+
+		// sendto(sock, message, sizeof(message), 0, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+		write(sock, message, strlen(message)); // 直接写
+		/* adr_sz = sizeof(from_adr);
+		str_len = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
+		*/
+		str_len = read(sock, message, sizeof(message) - 1); // 直接读
+		message[str_len] = 0;
+		printf("Message from server: %s", message);
+	}
+	close(sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
